0a583990e642a97749d8bf3bf069d307ce1d2057
Saidur Rahman <swesaon@gmail.com>
2018-09-19 15:29:18 +0600

    installation of reactstrap

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './Header.css';
 
 class Header extends Component {
   render() {
     return (
       <div className="app-home-header">
         <h1>Header</h1>
+        <button color="primary">Log Out</button>
       </div>
     );
   }
